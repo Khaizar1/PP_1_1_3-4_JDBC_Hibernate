f194aafaecb0764447c3ed3ede4e8eed47f60760
Khaizar1 <khaizar@mail.ru>
2022-07-09 14:50:15 +0300

    Попытка номер один

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
+
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Util util;
+    private final Connection connection;
+
+    private Util() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String userName = "root";
+            String password = "0000";
+            String dbName = "mydb";
+            String hostName = "localhost";
+            String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+            this.connection = DriverManager.getConnection(connectionURL, userName, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static Util getInstance() throws SQLException {
+        if (util == null) {
+            util = new Util();
+        } else if (util.getConnection().isClosed()) {
+            util = new Util();
+        }
+        return util;
+    }
 }
